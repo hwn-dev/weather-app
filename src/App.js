8271f6ad6fe5a8d10c68a0e96bfb11ae34e0eb05
Harry Neale <harryneale@Harrys-MBP.broadband>
2020-11-17 22:53:39 +0000

    added header, heading and form components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
 import logo from './logo.svg';
 import './App.css';
+import Header from './styles/HeaderStyle';
+import Heading from './styles/HeadingStyle';
+import Form from './styles/Form';
 
-function App() {
+const AppWrapper = styled.div`
+  width:100vw;
+  height:100vh;
+`;
+
+class App extends Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <AppWrapper>
+      <Header>
+        <Heading>
+          WEATHER TODAY
+        </Heading>
+        <Form>
+            <input
+              type="text"
+              id="search"
+              name="search"
+              placeholder="Search town..."
+            />
+        </Form>
+      </Header>
+          
+
+
+
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Boo
         </p>
         <a
-          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          text="Yo"
         >
-          Learn React
+         Yo mumma
         </a>
-      </header>
-    </div>
+      </header> */}
+    </AppWrapper>
   );
+}
 }
 
 export default App;
---
+++ src/styles/HeaderStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Header = styled.div`
+    width:100%;
+    height:125px;
+    position:fixed;
+    display: flex;
+    border-bottom: 3px solid blue;
+    background-color:black;
+`;
+
+export default Header;
