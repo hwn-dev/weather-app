c64d9fe0908fce035da8de1ebb395cef4c2f4c7c
Harry Neale <harry.neale@vodafone.com>
2021-01-13 11:56:45 +0000

    moved cards into form and deleted cards

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,8 +3,28 @@ import React, { useState } from "react";
 const Form = () => {
   const [location, setLocation] = useState("");
 
+  const api = {
+    key: "4e48b51081eb15cffe3586dba85c9297",
+    base: "http://api.weatherstack.com/current?access_key=",
+  };
+
+  const labels = ["Max temp", "Min temp"];
+
+  const degrees = ["&deg;"];
+
+  function GetWeather() {
+    fetch(`${api.base}${api.key}&query=${Form.location}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (response) {
+        console.log(response.temperature);
+      });
+  }
+
   function handleSubmit(e) {
     e.preventDefault();
+    GetWeather();
   }
 
   function handleChange(e) {
@@ -12,20 +32,32 @@ const Form = () => {
   }
 
   return (
-    <div className="search-bar">
-      <form onSubmit={handleSubmit}>
-        <input
-          className="search-form"
-          type="text"
-          name="search"
-          autocorrect="off"
-          value={location}
-          placeholder="Search..."
-          onChange={handleChange}
-        ></input>
-      </form>
-      <div id="output">
-        <h4>{location}</h4>
+    <div>
+      <div className="search-bar">
+        <form onSubmit={handleSubmit}>
+          <input
+            className="search-form"
+            type="text"
+            name="search"
+            autoCorrect="off"
+            value={location}
+            placeholder="Search..."
+            onChange={handleChange}
+          ></input>
+        </form>
+      </div>
+      <div className="cards">
+        <h1>{location}</h1>
+        <span className="temp">
+          {}
+          <span className="metric"></span>
+          <p>placeholder</p>
+        </span>
+        <span className="temp">
+          {}
+          <span className="metric"></span>
+          <p>placeholder</p>
+        </span>
       </div>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Form from "./Form";
-import Cards from "./Cards";
 
 const Main = () => {
   return (
     <div className="main">
       <div className="control">
         <Form />
-        <Cards />
       </div>
     </div>
   );
